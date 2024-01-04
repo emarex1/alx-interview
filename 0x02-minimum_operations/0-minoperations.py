3a893994fe5e77af8dea19b4e180300c6936d79c
emarex1 <emmanuelawoh0@gmail.com>
2024-01-04 13:41:15 +0100

    added solution

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,19 +7,18 @@ def minOperations(n):
     minOperations
     Gets fewest # of operations needed to result in exactly n H characters
     """
-    # all outputs should be at least 2 char: (min, Copy All => Paste)
-    if (n < 2):
+    if n == 1:
         return 0
-    ops, root = 0, 2
-    while root <= n:
-        # if n evenly divides by root
-        if n % root == 0:
-            # total even-divisions by root = total operations
-            ops += root
-            # set n to the remainder
-            n = n / root
-            # reduce root to find remaining smaller vals that evenly-divide n
-            root -= 1
-        # increment root until it evenly-divides n
-        root += 1
-    return ops
+    
+    # Initialize variables
+    result = n  # At most, n operations are needed
+    divisor = 2  # Start dividing by 2
+    
+    while divisor <= n:
+        if n % divisor == 0:
+            # If n is divisible by the divisor, check the operations needed for divisor copies
+            result = min(result, minOperations(n // divisor) + divisor)
+        
+        divisor += 1
+    
+    return result
